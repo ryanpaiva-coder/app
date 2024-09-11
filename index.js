bdde30fcb09e0b0fdb0828c077f23b99c316c84d
Ryan Pedro <paiva.code@gmail.com>
2024-09-11 10:10:02 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// hello world
-let mensagem = "hello world!"
-console.log(mensagem);
+let meta = {
+    value: "Ler um livro por mês",
+    checked: true,
+}
+
+let metas = [
+    meta,
+    {
+        value: "Caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+console.log(metas[1].value)
